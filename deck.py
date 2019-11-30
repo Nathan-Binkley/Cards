c5e42ab03930eb700627ebcf938dbbd2e0bb83b3
Nathan-Binkley <nbink2@gmail.com>
2019-11-29 20:28:46 -0500

    Creation of deck object

--- deck.py
+++ deck.py
@@ -1,1 +1,16 @@
-import random
+class deck(object):
+    """Class for the creation of a deck and cards"""
+
+    def __init__(self, jokers = False):
+        self.deck = []
+        for i in range(13):
+            self.deck.append(i+"S") # Spades
+            self.deck.append(i+"H") # Hearts
+            self.deck.append(i+"C") # Clubs
+            self.deck.append(i+"D") # Diamonds
+            # 11 = Jack
+            # 12 = Queen
+            # 13 = King
+        if jokers:
+            self.deck.append("Joker")
+            self.deck.append("Joker")
